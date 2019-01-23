89245da247bf01de2c8faca4132c3e7cb690fc23
aishabn <abn.226@gmail.com>
2019-01-23 17:58:11 +0300

    Backend-V1

--- api/models.py
+++ api/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Product(models.Model):
+	category_choices = [('indoor','Indoor'),('outdoor','Outdoor'),('apparel','Apparel'),('footwear','Footwear'),('fitness','Fitness'),('sports','Sports')]
+
+	name = models.CharField(max_length=200)
+	price = models.DecimalField(max_digits=8, decimal_places=3)
+	description = models.TextField()
+	img = models.ImageField()
+	category = models.CharField(max_length=200, choices=category_choices)
+	brand = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
+
+class CartItem(models.Model):
+	item = models.ForeignKey(Product, on_delete=models.CASCADE)
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+
--- api/serializers.py
+++ api/serializers.py
@@ -1,17 +1,43 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from rest_framework import serializers
+
+from .models import Product 
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    class Meta:
-        model = User
-        fields = ['username', 'password']
+	password = serializers.CharField(write_only=True)
+	class Meta:
+		model = User
+		fields = ['username', 'password']
 
-    def create(self, validated_data):
-        username = validated_data['username']
-        password = validated_data['password']
-        new_user = User(username=username)
-        new_user.set_password(password)
-        new_user.save()
-        return validated_data
+	def create(self, validated_data):
+		username = validated_data['username']
+		password = validated_data['password']
+		new_user = User(username=username)
+		new_user.set_password(password)
+		new_user.save()
+		return validated_data
+
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ['username', 'first_name', 'last_name', 'email', ]
+
+class ProductListSerializer(serializers.ModelSerializer):
+	detail = serializers.HyperlinkedIdentityField(
+		view_name = "api-detail",
+		lookup_field = "id",
+		lookup_url_kwarg = "product_id"
+		)
+	
+	class Meta:
+		model = Product
+		fields = ['id','name', 'price', 'img', 'detail']
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Product
+		fields = '__all__'
